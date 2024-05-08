4a8a27e9e5aa7f6f8a1801f4abef701874e74b33
Mostapha <darsh.taha12382@gmail.com>
2024-05-08 11:30:56 +0300

    Dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
+import Dashboard from './Dashboard/dashboard';
 
 export default function Home() {
   return (
     <>
-    <div className="lg:ml-[290px]">
-     <h1>
-     Stay tuned
-     </h1>
-    </div>
-
+      <div className="lg:ml-[290px]">
+        <Dashboard/>
+      </div>
     </>
   );
 }
