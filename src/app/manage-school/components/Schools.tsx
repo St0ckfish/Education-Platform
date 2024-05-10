5ab65e9eaf8b175a1bf2fb4692eda62920d75837
Mostapha <darsh.taha12382@gmail.com>
2024-05-10 11:54:43 +0300

    Features

--- src/app/manage-school/components/Schools.tsx
+++ src/app/manage-school/components/Schools.tsx
@@ -8,20 +8,20 @@ const Schools = () => {
     return (
         <>
             <div className="relative mt-10 overflow-x-auto bg-transparent sm:rounded-lg max-[1200px]:w-screen h-screen">
-                    <div className="flex justify-between max-[502px]:grid max-[502px]:justify-center text-center">
-                        <div className="mb-3">
-                            <label htmlFor="icon" className="sr-only">Search</label>
-                            <div className="relative min-w-72 md:min-w-80">
-                                <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4">
-                                    <svg className="flex-shrink-0 size-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8" /><path d="m21 21-4.3-4.3" /></svg>
-                                </div>
-                                <input type="text" id="icon" name="icon" className="py-2  outline-none border-2 px-4 ps-11 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" placeholder="Search" />
+                <div className="flex justify-between max-[502px]:grid max-[502px]:justify-center text-center">
+                    <div className="mb-3">
+                        <label htmlFor="icon" className="sr-only">Search</label>
+                        <div className="relative min-w-72 md:min-w-80">
+                            <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4">
+                                <svg className="flex-shrink-0 size-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8" /><path d="m21 21-4.3-4.3" /></svg>
                             </div>
-                        </div>
-                        <div className="flex justify-center">
-                            <Link href="/add-new-school" className="px-4 py-2 whitespace-nowrap rounded-xl bg-[#3E5AF0] hover:bg-[#4a5cc5] hover:shadow-xl mb-5 mr-3 text-white text-[18px] w-[180px] ease-in font-semibold duration-300">+ Add new school</Link>
+                            <input type="text" id="icon" name="icon" className="py-2  outline-none border-2 px-4 ps-11 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" placeholder="Search" />
                         </div>
                     </div>
+                    <div className="flex justify-center">
+                        <Link href="/add-new-school" className="px-4 py-2 whitespace-nowrap rounded-xl bg-[#3E5AF0] hover:bg-[#4a5cc5] hover:shadow-xl mb-5 mr-3 text-white text-[18px] w-[180px] ease-in font-semibold duration-300">+ Add new school</Link>
+                    </div>
+                </div>
                 <div className="overflow-auto relative shadow-md sm:rounded-lg">
                     <table className="w-full overflow-x-auto text-sm text-left rtl:text-right text-gray-500 ">
                         <thead className="text-xs text-gray-700 uppercase bg-[#daeafb] ">
@@ -51,6 +51,9 @@ const Schools = () => {
                                 </th>
                                 <th scope="col" className="px-6 py-3 whitespace-nowrap">
                                     Actions
+                                </th>
+                                <th scope="col" className="px-6 py-3 whitespace-nowrap">
+                                    Features
                                 </th>
                                 <th scope="col" className="px-6 py-3 whitespace-nowrap">
                                     Admin
@@ -84,6 +87,9 @@ const Schools = () => {
                                 </td>
                                 <td className="px-6 py-4 whitespace-nowrap">
                                     Deactivate
+                                </td>
+                                <td  className="px-6 py-4 whitespace-nowrap">
+                                    <Link href="/features"  className="font-medium text-blue-600 hover:underline">Feature</Link>
                                 </td>
                                 <td className="px-6 py-4 whitespace-nowrap">
                                     <Link href="/admin" className="font-medium text-blue-600 hover:underline">View</Link>
