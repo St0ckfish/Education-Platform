c1f86713473e07ad04e96829fbe39fddf88db4f4
khaledsayed273 <khaledsayed273@gmail.com>
2024-07-24 11:09:44 +0300

    finished auth login

--- src/app/manage-school/components/Schools.tsx
+++ src/app/manage-school/components/Schools.tsx
@@ -37,7 +37,7 @@ const Schools = () => {
                     </div>
                     <div className="flex justify-center">
                         <Link href="/add-new-school" className="px-4 py-2 whitespace-nowrap rounded-xl bg-[#3E5AF0] hover:bg-[#4a5cc5] hover:shadow-xl mb-5 mr-3 text-white text-[18px] w-[180px] ease-in font-semibold duration-300">+ Add new school</Link>
-                    </div>
+                    </div> 
                 </div>
                 <div className="overflow-auto relative shadow-md sm:rounded-lg">
                     <table className="w-full overflow-x-auto text-sm text-left rtl:text-right text-gray-500 ">
