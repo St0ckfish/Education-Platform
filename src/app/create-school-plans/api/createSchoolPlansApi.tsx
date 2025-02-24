504da3cec6520cee8b745f93277f670459f3ebf7
a7medabdelfadil <a7med.abdelfadeel@gmail.com>
2025-02-24 13:42:19 +0200

    fix create school plan

--- src/app/create-school-plans/api/createSchoolPlansApi.tsx
+++ src/app/create-school-plans/api/createSchoolPlansApi.tsx
@@ -1,33 +1,34 @@
 import { baseUrl } from "@/api/axios";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-
 export const createSchoolPlanApi = createApi({
-    reducerPath: "createSchoolPlans",
-    baseQuery: fetchBaseQuery({
-        baseUrl: baseUrl
+  reducerPath: "createSchoolPlans",
+  baseQuery: fetchBaseQuery({
+    baseUrl: baseUrl,
+  }),
+  endpoints: (builder) => ({
+    getPermissions: builder.query({
+      query: (token: string) => ({
+        url: "management/school/plan/permissions",
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }),
     }),
-    endpoints: (builder) => ({
-        getPermissions: builder.query({
-            query: (token: string) => ({
-                url: "management/school/plan/permissions",
-                method: "GET",
-                headers: {
-                    "Authorization": `Bearer ${token}`
-                }
-            })
-        }),
-        addSchoolPlan: builder.mutation({
-            query: ({ token, body }) => ({
-                url: "management/school/plan",
-                method: "POST",
-                headers: {
-                    "Authorization": `Bearer ${token}`
-                },
-            })
-        })
-    })
-})
+    addSchoolPlan: builder.mutation({
+      query: ({ token, body }) => ({
+        url: "management/school/plan",
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body,
+      }),
+    }),
+  }),
+});
 
-
-export const { useGetPermissionsQuery , useAddSchoolPlanMutation } = createSchoolPlanApi
+export const { useGetPermissionsQuery, useAddSchoolPlanMutation } =
+  createSchoolPlanApi;
